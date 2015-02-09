fc2cf4588e90e4cca2c8050133e1da0df95ebc75
fatesinger <jigoulee@gmail.com>
2015-02-09 21:05:36 +0800

    remove default ad

--- content-single.php
+++ content-single.php
@@ -2,11 +2,6 @@
 <header class="angela-post-header">
 <h2 class="angela-title"><?php the_title();?></h2>
 </header>
-<div class="v-overflowHidden v-textAlignCenter angela-ad"><script type="text/javascript">
-    /*6:5 创建于 2015-01-07*/
-    var cpro_id = "u1896072";
-</script>
-<script src="http://cpro.baidustatic.com/cpro/ui/cm.js" type="text/javascript"></script></div>
 <div class="entry-content">
 <?php the_content();?>
 </div>
