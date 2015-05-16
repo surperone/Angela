ee63fa6501c150035aed242400f1542022d200af
bigfa <tinyglim@gmail.com>
2015-05-16 12:58:28 +0800

    1.0.2

--- README.md
+++ README.md
@@ -10,8 +10,9 @@
 
 编辑`footer.php`
 
-	$jssdk = new JSSDK("yourAppID", "yourAppSecret");
+	define('APPID','');//yourAppID
+	define('APPSECRET','');//yourAppSecret
 
-替换你的`appid`和`appsecret`即可
+填写你的`appid`和`appsecret`即可
 
 注意公众帐号设置中要把你的域名加入到安全域名中	
--- footer.php
+++ footer.php
@@ -8,69 +8,73 @@
  * @since Anglea 1.0
  */
 ?>
-    <footer id="colophon" class="angelaContainer angelaFooter" role="contentinfo">
-        <div class="site-info">
-            <a href="http://fatesinger.com/74850">Angela Theme</a>
-        </div><!-- .site-info -->
-    </footer><!-- .site-footer -->
+<footer id="colophon" class="angelaContainer angelaFooter" role="contentinfo">
+    <div class="site-info">
+        <a href="http://fatesinger.com/74850">Angela Theme</a>
+    </div><!-- .site-info -->
+</footer><!-- .site-footer -->
 </div>
 <div class="v-hide"></div><!-- 统计代码 -->
 <?php wp_footer(); ?>
 <?php
-$jssdk = new JSSDK("yourAppID", "yourAppSecret");
-$signPackage = $jssdk->GetSignPackage();
-if(is_single()){
-	global $post;
-	$title = get_the_title();
-	$url = get_permalink();
-	$image = angela_get_background_image($post->ID);
-	$des = get_the_excerpt();
-} else {
-	$title = get_bloginfo( 'name', 'display' );
-	$url = home_url();
-	$image = '';
-	$des = get_bloginfo( 'description', 'display' );
-}
-?>
-<script src="http://res.wx.qq.com/open/js/jweixin-1.0.0.js"></script>
-<script>
-  wx.config({
-    debug: false,
-    appId: '<?php echo $signPackage["appId"];?>',
-    timestamp: <?php echo $signPackage["timestamp"];?>,
-    nonceStr: '<?php echo $signPackage["nonceStr"];?>',
-    signature: '<?php echo $signPackage["signature"];?>',
-    jsApiList: [
-      'onMenuShareTimeline','onMenuShareAppMessage'
-    ]
-  });
-  wx.ready(function () {
-    wx.onMenuShareAppMessage({
-    title: '<?php echo $title;?>', // 分享标题
-    desc: '<?php echo $des;?>', // 分享描述
-    link: '<?php echo $url;?>', // 分享链接
-    imgUrl: '<?php echo $image;?>', // 分享图标
-    type: '', // 分享类型,music、video或link，不填默认为link
-    dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
-    success: function () { 
-        // 用户确认分享后执行的回调函数
-    },
-    cancel: function () { 
-        
+define('APPID','');//yourAppID
+define('APPSECRET','');//yourAppSecret
+if( APPID && APPSECRET ){
+    $jssdk = new JSSDK(APPID, APPSECRET);
+    $signPackage = $jssdk->GetSignPackage();
+    if(is_single()){
+        global $post;
+        $title = get_the_title();
+        $url = get_permalink();
+        $image = angela_get_background_image($post->ID);
+        $des = get_the_excerpt();
+    } else {
+        $title = get_bloginfo( 'name', 'display' );
+        $url = home_url();
+        $image = '';
+        $des = get_bloginfo( 'description', 'display' );
     }
-});
-    wx.onMenuShareTimeline({
-    title: '<?php echo $title;?>', // 分享标题
-    link: '<?php echo $url;?>', // 分享链接
-    imgUrl: '<?php echo $image;?>', // 分享图标
-    success: function () { 
-        // 用户确认分享后执行的回调函数
-    },
-    cancel: function () { 
-        // 用户取消分享后执行的回调函数
-    }
-});
-  });
-</script>
+    ?>
+    <script src="http://res.wx.qq.com/open/js/jweixin-1.0.0.js"></script>
+    <script>
+        wx.config({
+            debug: false,
+            appId: '<?php echo $signPackage["appId"];?>',
+            timestamp: <?php echo $signPackage["timestamp"];?>,
+            nonceStr: '<?php echo $signPackage["nonceStr"];?>',
+            signature: '<?php echo $signPackage["signature"];?>',
+            jsApiList: [
+                'onMenuShareTimeline','onMenuShareAppMessage'
+            ]
+        });
+        wx.ready(function () {
+            wx.onMenuShareAppMessage({
+                title: '<?php echo $title;?>', // 分享标题
+                desc: '<?php echo $des;?>', // 分享描述
+                link: '<?php echo $url;?>', // 分享链接
+                imgUrl: '<?php echo $image;?>', // 分享图标
+                type: '', // 分享类型,music、video或link，不填默认为link
+                dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
+                success: function () {
+                    // 用户确认分享后执行的回调函数
+                },
+                cancel: function () {
+
+                }
+            });
+            wx.onMenuShareTimeline({
+                title: '<?php echo $title;?>', // 分享标题
+                link: '<?php echo $url;?>', // 分享链接
+                imgUrl: '<?php echo $image;?>', // 分享图标
+                success: function () {
+                    // 用户确认分享后执行的回调函数
+                },
+                cancel: function () {
+                    // 用户取消分享后执行的回调函数
+                }
+            });
+        });
+    </script>
+<?php } ?>
 </body>
 </html>
